e0d35382a49bea93464673602ad4d97063cc7623
textualbois <wivna360@hotmail.com>
2024-08-27 15:01:20 +0200

    added more cpp styled variable declaration and class initiaition

--- ex01/Contact.cpp
+++ ex01/Contact.cpp
@@ -1,49 +1,59 @@
 #include "Contact.hpp"
 
-Contact::Contact() {
-	this->isInitialized = false;
+Contact::Contact()
+	:	_isInitialized(false) {
 }
 
-Contact::Contact(const std::string firstName, const std::string lastName,
-				const std::string nickname, const std::string darkestSecret) {
-	this->firstName = firstName;
-	this->lastName = lastName;
-	this->nickname = nickname;
-	this->darkestSecret = darkestSecret;
-	this->isInitialized = true;
+Contact::Contact(const std::string& firstName, const std::string& lastName,
+				const std::string& nickname, const std::string& phoneNumber,
+				const std::string& darkestSecret)
+	:	_firstName(firstName),
+		_lastName(lastName),
+		_nickname(nickname),
+		_phoneNumber(phoneNumber),
+		_darkestSecret(darkestSecret),
+		_isInitialized(true) {
 }
 
 Contact::~Contact() {
 }
 
-void Contact::setFirstName(std::string firstName) {
-	this->firstName = firstName;
+void Contact::setFirstName(std::string& firstName) {
+	this->_firstName = firstName;
 }
 
-void Contact::setLastName(std::string lastName) {
-	this->lastName = lastName;
+void Contact::setLastName(std::string& lastName) {
+	this->_lastName = lastName;
 }
 
-void Contact::setNickname(std::string nickname) {
-	this->nickname = nickname;
+void Contact::setNickname(std::string& nickname) {
+	this->_nickname = nickname;
 }
 
-void Contact::setDarkestSecret(std::string darkestSecret) {
-	this->darkestSecret = darkestSecret;
+void Contact::setPhoneNumber(std::string& phoneNumber) {
+	this->_phoneNumber = phoneNumber;
+}
+
+void Contact::setDarkestSecret(std::string& darkestSecret) {
+	this->_darkestSecret = darkestSecret;
 }
 
 std::string Contact::getFirstName() {
-	return (this->firstName);
+	return (this->_firstName);
 }
 
 std::string Contact::getLastName() {
-	return (this->lastName);
+	return (this->_lastName);
 }
 
 std::string Contact::getNickname() {
-	return (this->nickname);
+	return (this->_nickname);
+}
+
+std::string Contact::getPhoneNumber() {
+	return (this->_phoneNumber);
 }
 
 std::string Contact::getDarkestSecret() {
-	return (this->darkestSecret);
+	return (this->_darkestSecret);
 }
--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -4,24 +4,28 @@
 class Contact {
 	public:
 		Contact();
-		Contact(const std::string firstName,
-				const std::string lastName,
-				const std::string nickname,
-				const std::string darkestSecret);
+		Contact(const std::string& firstName,
+				const std::string& lastName,
+				const std::string& nickname,
+				const std::string& phoneNumber,
+				const std::string& darkestSecret);
 		~Contact();
-		void setFirstName(std::string firstName);
-		void setLastName(std::string lastName);
-		void setNickname(std::string nickname);
-		void setDarkestSecret(std::string darkestSecret);
+		void setFirstName(std::string& firstName);
+		void setLastName(std::string& lastName);
+		void setNickname(std::string& nickname);
+		void setPhoneNumber(std::string& phoneNumber);
+		void setDarkestSecret(std::string& darkestSecret);
 		std::string getFirstName();
 		std::string getLastName();
 		std::string getNickname();
+		std::string getPhoneNumber();
 		std::string getDarkestSecret();
-		bool isInitialized;
+		bool _isInitialized;
 
 	private:
-		std::string firstName;
-		std::string lastName;
-		std::string nickname;
-		std::string darkestSecret;
+		std::string _firstName;
+		std::string _lastName;
+		std::string _nickname;
+		std::string _phoneNumber;
+		std::string _darkestSecret;
 };
