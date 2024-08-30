4ff64c2d5af50b2b733b9fff49dedb9bad7996ea
textualbois <wivna360@hotmail.com>
2024-08-30 12:39:45 +0200

    ex02 bones

---
+++ ex02/Account.cpp
@@ -0,0 +1,48 @@
+#include "Account.hpp"
+
+Account::Account(const int& initital_deposit )
+	:	_(true){
+}
+
+Account::~Account() {
+}
+
+void Contact::setFirstName(std::string& firstName) {
+	this->_firstName = firstName;
+}
+
+void Contact::setLastName(std::string& lastName) {
+	this->_lastName = lastName;
+}
+
+void Contact::setNickname(std::string& nickname) {
+	this->_nickname = nickname;
+}
+
+void Contact::setPhoneNumber(std::string& phoneNumber) {
+	this->_phoneNumber = phoneNumber;
+}
+
+void Contact::setDarkestSecret(std::string& darkestSecret) {
+	this->_darkestSecret = darkestSecret;
+}
+
+std::string Contact::getFirstName() {
+	return (this->_firstName);
+}
+
+std::string Contact::getLastName() {
+	return (this->_lastName);
+}
+
+std::string Contact::getNickname() {
+	return (this->_nickname);
+}
+
+std::string Contact::getPhoneNumber() {
+	return (this->_phoneNumber);
+}
+
+std::string Contact::getDarkestSecret() {
+	return (this->_darkestSecret);
+}
